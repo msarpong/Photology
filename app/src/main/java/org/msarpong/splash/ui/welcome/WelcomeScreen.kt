b33a94c9d12748ab27ff6fc889e5ff440ba95f27
msarpong <msarpong1991@gmail.com>
2020-07-20 19:45:37 +0200

    Added logic to the skip button

--- app/src/main/java/org/msarpong/splash/ui/welcome/WelcomeScreen.kt
+++ app/src/main/java/org/msarpong/splash/ui/welcome/WelcomeScreen.kt
@@ -1,12 +1,29 @@
 package org.msarpong.splash.ui.welcome
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import org.msarpong.splash.R
+import org.msarpong.splash.ui.main.MainScreen
 
 class WelcomeScreen : AppCompatActivity() {
+
+    private lateinit var skipButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.welcome_screen)
+
+        setupViews()
+    }
+
+    private fun setupViews() {
+        skipButton = findViewById(R.id.skip_button)
+        skipButton.setOnClickListener {
+            val main = Intent(this, MainScreen::class.java)
+            startActivity(main)
+            finish()
+        }
     }
 }

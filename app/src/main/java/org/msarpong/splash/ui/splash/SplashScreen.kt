0c6e210f4b1ecf5ae74de2d8cd0d3f58bf1dc37f
msarpong <msarpong1991@gmail.com>
2020-07-20 18:02:43 +0200

    Added runnable function

--- app/src/main/java/org/msarpong/splash/ui/splash/SplashScreen.kt
+++ app/src/main/java/org/msarpong/splash/ui/splash/SplashScreen.kt
@@ -1,12 +1,28 @@
 package org.msarpong.splash.ui.splash
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import org.msarpong.splash.R
+import org.msarpong.splash.ui.welcome.WelcomeScreen
+
+const val WELCOME_TIME = 4000L
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.splash_screen)
+        goToWelcome()
+    }
+
+    private fun goToWelcome() {
+        val runnableWelcome = Runnable {
+            val main = Intent(this, WelcomeScreen::class.java)
+            startActivity(main)
+            finish()
+        }
+        Handler().postDelayed(runnableWelcome, WELCOME_TIME)
+
     }
 }

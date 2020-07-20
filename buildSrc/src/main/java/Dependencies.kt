bf7b8be0ae15dba4516ce891b49eadbe3b2199b1
msarpong <msarpong1991@gmail.com>
2020-07-20 16:58:54 +0200

    Added dependency manager (buildSrc)

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,13 @@
+object Versions {
+    const val kotlin = "1.3.72"
+}
+
+object Dependencies {
+    const val kotlinLibrary = "org.jetbrains.kotlin:kotlin-stdlib:${Versions.kotlin}"
+    const val androidXCore = "androidx.core:core-ktx:1.3.0"
+    const val androidXAppCompat = "androidx.appcompat:appcompat:1.1.0"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:1.1.3"
+    const val jUnit = "junit:junit:4.12"
+    const val androidJUnit = "androidx.test.ext:junit:1.1.1"
+    const val androidEspresso = "androidx.test.espresso:espresso-core:3.2.0"
+}

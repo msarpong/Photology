4834e18895c268461282535295b06b57ab9fe17d
msarpong <msarpong1991@gmail.com>
2020-07-20 18:03:55 +0200

    Added Welcome screen

---
+++ app/src/main/java/org/msarpong/splash/ui/welcome/WelcomeScreen.kt
@@ -0,0 +1,12 @@
+package org.msarpong.splash.ui.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import org.msarpong.splash.R
+
+class WelcomeScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome_screen)
+    }
+}
